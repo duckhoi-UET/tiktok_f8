c9b6224bc2a63de56d598a1c8ccd53277ba70290
khoind <khoind@rikkeisoft.com>
2023-12-11 16:25:20 +0700

    use effect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Content from "./Content";
+import { useState } from "react";
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <div className="App">
-      <Content />
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      {toggle && <Content />}
     </div>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,39 @@
-import React from "react";
+import { useEffect, useState } from "react";
+
+const tabs = ["posts", "comments", "albums"];
 
 function Content() {
-  return <h1>Đây là content</h1>;
+  const [type, setType] = useState("");
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    if (type) {
+      fetch(`https://jsonplaceholder.typicode.com/${type}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setPosts(data);
+        });
+    }
+  }, [type]);
+
+  return (
+    <div>
+      {tabs.map((tab) => (
+        <button
+          key={tab}
+          style={type === tab ? { color: "#fff", backgroundColor: "red" } : {}}
+          onClick={() => setType(tab)}
+        >
+          {tab}
+        </button>
+      ))}
+      <input value={type} />
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title || post.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Content;
