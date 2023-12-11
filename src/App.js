063e830db3f6fb4d118fd823adab3adb6ee44058
khoind <khoind@rikkeisoft.com>
2023-12-11 15:05:58 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Content from "./Content";
+
+function App() {
+  return (
+    <div className="App">
+      <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Content.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Content() {
+  return <h1>Đây là content</h1>;
+}
+
+export default Content;
